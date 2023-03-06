5108fb3443e0416a20ebac6ac73d2acbde77d864
santiagohewett <>
2023-03-06 16:11:20 +0000

    Check to see if it is failing

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2023 Santiago Hewett All rights reserved
+//
+// Created by: Santiago Hewett
+// Created on: Mar 2023
+// This file contains the JS functions for index.html
+
+function myButtonClicked () {
+  // this function displays "Hello, World!"
+  
+  document.getElementById("hello-world").innerHTML = "<h5>I coded this in Javascript!</h5>"
+}
